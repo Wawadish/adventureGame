6f505fe4dab48361d15a2eced048c735681fda5f
Flowkey <wawadish@gmail.com>
2018-03-03 19:23:24 -0500

    Working Version of Map Generator
    You can start using this for your movement Zimu

---
+++ Map/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map map = new Map();  //creating the map
+        map.generate();  //generating the map
+        map.draw();  //displaying the map
+    }
+}
